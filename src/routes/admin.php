07dfee7a3e952585ce5ce0cc89de69e1e97a0ac7
mari c <special_mc@mailo.com>
2024-05-30 18:46:57 +0200

    update null

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -8,8 +8,7 @@ $router->addMatchTypes(['uuid' => '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-
 // Users
 $router->map('GET|POST', $admin . '/connexion', 'users/admin_login', 'login');
 $router->map('GET|POST', $admin . '/mot-de-passe-oublie', '', 'lostPassword');
-$router->map('GET|POST', $admin . '/deconnexion', 'users/admin_logout', 'logout');
-$router->map('GET|POST', $admin . '/utilisateurs/tableau-de-bord', 'users/admin_dashboard', 'dashboard');
+$router->map('GET|POST', $admin . '/utilisateurs', 'users/admin_listUsers', 'users');
 $router->map('GET|POST', $admin . '/utilisateur', 'users/admin_users', 'users');
 $router->map('GET|POST', $admin . '/utilisateur/editer', 'users/admin_edit', 'editUser');
 // $router->map('GET|POST', $admin . '/utilisateur/editer', 'users/admin_edit', 'addUser');
